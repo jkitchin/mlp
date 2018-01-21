87fb56c7973c9bed232ada52d51e4265503d69f6
John Kitchin <jkitchin@andrew.cmu.edu>
2018-01-21 10:13:36 -0800

    add example asserts for testing.

--- mlp/tests/test_sanity.py
+++ mlp/tests/test_sanity.py
@@ -1,3 +1,8 @@
+"""Sanity tests.
+
+These should just work and verify that the basic Python environment is setup.
+Also, they remind me of the unittest structure.
+"""
 import unittest
 import ase
 
@@ -5,11 +10,48 @@ class TestSanity(unittest.TestCase):
     def test0(self):
         self.assertTrue(True)
         self.assertFalse(False)
-        self.assertEquals(1, 1)
+
+        self.assertEqual(1, 1)
+        self.assertAlmostEqual(0, 1e-6, 5)
+
+        self.assertNotEqual(1, 2)
+        self.assertNotAlmostEqual(0, 1e-6, 6)
+        
         self.assertGreater(1, 0)
+        self.assertGreaterEqual(0, 0)
+        self.assertGreaterEqual(1, 0)
+
         self.assertLess(0, 1)
         self.assertLessEqual(0, 0)
         self.assertLessEqual(0, 1)
+
+        self.assertIn(1, [0, 1])
+        self.assertNotIn(2, [0, 1])
+        
+        self.assertIsNone(None)
+        self.assertIsNotNone(True)
+
+        self.assertSequenceEqual([0, 1], [0, 1])
+        self.assertListEqual([0, 1], [0, 1])
+
+        self.assertDictEqual({0: 'test', 1: 'tree'},
+                             {1: 'tree', 0: 'test'})
+
+        self.assertTupleEqual((1, 2, 3), (1, 2, 3))
+
+
+        self.assertSetEqual(set([0, 1]), set([1, 1, 0]))
+
+        s = '''a long 
+multiline
+string'''
+        self.assertMultiLineEqual(s, s)
+        
+        with self.assertRaises(ZeroDivisionError):
+            1 / 0
+
+        self.assertCountEqual([0, 1, 1], [1, 0, 1])
+
         
     def test1(self):
         from mlp.sanity import sanity0
