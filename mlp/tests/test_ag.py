3df7628110dedfe8f7f9df47a8f44dfb30296cd5
John Kitchin <jkitchin@andrew.cmu.edu>
2018-01-21 12:48:18 -0800

    clean trailing whitespace, redo test for different structures

--- mlp/tests/test_ag.py
+++ mlp/tests/test_ag.py
@@ -8,10 +8,10 @@ from mlp.ag.neighborlist import get_distances
 from mlp.ag.lennardjones import energy, forces, stress
 
 
-
 class TestNeighborList(unittest.TestCase):
 
     def test0(self):
+        "Check a variety of repeats with different symmetries."
         a = 3.6
         for cutoff_radius in np.linspace(a / 2, 5 * a, 10):
             for rep in ((1, 1, 1),
@@ -20,45 +20,48 @@ class TestNeighborList(unittest.TestCase):
                         (1, 1, 2),
                         (1, 2, 2),
                         (2, 1, 1),
-                        (2, 2, 1),                        
+                        (2, 2, 1),
                         (2, 2, 2),
                         (1, 2, 3)):
                 atoms = bulk('Cu', 'fcc', a=a).repeat(rep)
 
                 nl = NeighborList([cutoff_radius / 2] * len(atoms), skin=0.01,
                                   self_interaction=False, bothways=True)
-                nl.update(atoms)      
-                nns_ase = [len(nl.get_neighbors(i)[0]) for i in range(len(atoms))]
+                nl.update(atoms)
+                nns_ase = [len(nl.get_neighbors(i)[0])
+                           for i in range(len(atoms))]
 
                 d = get_distances(atoms.positions, atoms.cell, cutoff_radius)
                 inds = (d <= (cutoff_radius + 0.01)) & (d > 0.00)
                 nns = inds.sum((1, 2))
-                
+
                 self.assertTrue(np.all(nns_ase == nns))
 
 
 class TestLennardJones(unittest.TestCase):
-    def test_energy(self):
+    def test_fcc(self):
+        "Check structures and repeats with different symmetries."
+        for struct in ['fcc', 'bcc', 'diamond']:
+            for repeat in [(1, 1, 1),
+                           (1, 1, 2),
+                           (1, 2, 1),
+                           (2, 1, 1),
+                           (1, 2, 3),
+                           (2, 2, 2)]:
+                atoms = bulk('Cu', struct, a=3.7).repeat(repeat)
+                atoms.rattle(0.02)
+                atoms.set_calculator(LennardJones())
 
-        atoms = bulk('Cu', 'fcc', a=3.7).repeat((2, 2, 1))
-        atoms.rattle(0.02)
-        atoms.set_calculator(LennardJones())
+                ase_energy = atoms.get_potential_energy()
+                lj_energy = energy({}, atoms.positions, atoms.cell)
 
-        ase_energy = atoms.get_potential_energy()
-        lj_energy = energy({}, atoms.positions, atoms.cell)
+                self.assertAlmostEqual(ase_energy, lj_energy)
 
-        self.assertAlmostEqual(ase_energy, lj_energy)
+                lj_forces = forces({}, atoms.positions, atoms.cell)
+                self.assertTrue(np.allclose(atoms.get_forces(),
+                                            lj_forces))
 
-        lj_forces = forces({}, atoms.positions, atoms.cell)
-        self.assertTrue(np.allclose(atoms.get_forces(),
-                                     lj_forces))
+                lj_stress = stress({}, atoms.positions, atoms.cell)
 
-        lj_stress = stress({}, atoms.positions, atoms.cell)
-        
-        self.assertTrue(np.allclose(atoms.get_stress(),
-                                     lj_stress))
-
-
-
-        
-        
+                self.assertTrue(np.allclose(atoms.get_stress(),
+                                            lj_stress))
