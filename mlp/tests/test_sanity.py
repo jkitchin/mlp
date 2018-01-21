fb77f5f0c96dc1f4c1bf2a8da1709bab43a57a7a
John Kitchin <jkitchin@andrew.cmu.edu>
2018-01-20 20:09:05 -0800

    add sanity tests

--- mlp/tests/test_sanity.py
+++ mlp/tests/test_sanity.py
@@ -4,7 +4,13 @@ import ase
 class TestSanity(unittest.TestCase):
     def test0(self):
         self.assertTrue(True)
-
+        self.assertFalse(False)
+        self.assertEquals(1, 1)
+        self.assertGreater(1, 0)
+        self.assertLess(0, 1)
+        self.assertLessEqual(0, 0)
+        self.assertLessEqual(0, 1)
+        
     def test1(self):
         from mlp.sanity import sanity0
         self.assertTrue(sanity0() == 2)
