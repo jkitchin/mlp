192affbdb993da70e8f79af055d9d343f1f81592
John Kitchin <jkitchin@andrew.cmu.edu>
2018-01-21 06:48:34 -0800

    changed to read some information

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-# Copyright 2015, John Kitchin
+# Copyright 2018, John Kitchin
 # (see accompanying license files for details).
-from setuptools import setup
 
+from setuptools import setup, find_packages
+
+with open('README.org') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+    
 setup(name='mlp',
-      version='0.01',
-      description='python computations in science and engineering',
+      version='0.0.1',
+      description='TODO',
+      long_description=readme,
+      author='John Kitchin',
+      author_email='jkitchin@andrew.cmu.edu',
       url='http://github.com/jkitchin/mlp',
-      maintainer='John Kitchin',
-      maintainer_email='jkitchin@andrew.cmu.edu',
-      license='GPL',
-      platforms=['linux'],
-      packages=['mlp'],
+      license=license,
       setup_requires=['nose>=1.0'],
       data_files=['requirements.txt', 'LICENSE'],
-      long_description='''TODO''')
+      packages=find_packages(exclude=('tests', 'docs')))
 
 # python setup.py register to setup user
 # to push to pypi - (shell-command "python setup.py sdist upload")
