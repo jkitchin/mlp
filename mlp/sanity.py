d0eb37fb8866a0726e109b549c6c457f8664963b
John Kitchin <jkitchin@andrew.cmu.edu>
2018-01-21 12:56:01 -0800

    pep8 fixes

--- mlp/sanity.py
+++ mlp/sanity.py
@@ -6,10 +6,13 @@ This contains simple functions that should work.
 import ase
 import tensorflow as tf
 
+
 def sanity0():
     """Sanity check function to make sure we are running tests."""
     from ase.build import bulk
+    print(ase.version)
     atoms = bulk('Cu', 'fcc', a=3.6).repeat((2, 1, 1))
+    atoms.get_volume()
     # This sanity function just returns 2
     return 2
 
@@ -17,4 +20,3 @@ def sanity0():
 def sanity1():
     a = tf.constant(1)
     return 2 * a
-    
