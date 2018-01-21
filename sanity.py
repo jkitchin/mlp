84273ea689ba360dccfaee62e963a3963fb781f0
John Kitchin <jkitchin@andrew.cmu.edu>
2018-01-21 06:48:20 -0800

    moved into mlp

--- sanity.py
+++
@@ -1,7 +0,0 @@
-import ase
-
-def sanity0():
-    """Sanity check function to make sure we are running tests."""
-    from ase.build import bulk
-    atoms = bulk('Cu', 'fcc', a=3.6).repeat((2, 1, 1))
-    return 2
