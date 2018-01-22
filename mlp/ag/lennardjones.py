84a9e8992522c5381bddd4c987b8ff2029d8c3e2
John Kitchin <jkitchin@andrew.cmu.edu>
2018-01-21 16:28:57 -0800

    add better docstrings

--- mlp/ag/lennardjones.py
+++ mlp/ag/lennardjones.py
@@ -7,7 +7,24 @@ from mlp.ag.neighborlist import get_distances
 
 
 def energy(params, positions, cell, strain=np.zeros((3, 3))):
-    "Compute the energy of a Lennard-Jones system."
+    """Compute the energy of a Lennard-Jones system.
+
+    Parameters
+    ----------
+
+    params : dictionary of paramters.
+      Defaults to {'sigma': 1.0, 'epsilon': 1.0}
+
+    positions : array of floats. Shape = (natoms, 3)
+
+    cell: array of unit cell vectors. Shape = (3, 3)
+
+    strain: array of strains to apply to cell. Shape = (3, 3)
+
+    Returns
+    -------
+    energy : float
+    """
 
     sigma = params.get('sigma', 1.0)
     epsilon = params.get('epsilon', 1.0)
@@ -28,17 +45,51 @@ def energy(params, positions, cell, strain=np.zeros((3, 3))):
     c12 = c6**2
     energy += np.sum(4 * epsilon * (c12 - c6))
 
+    # get_distances double counts the interactions, so we divide by two.
     return energy / 2
 
 
 def forces(params, positions, cell):
-    "Compute the forces of a Lennard-Jones system."
+    """Compute the forces of a Lennard-Jones system.
+
+    Parameters
+    ----------
+
+    params : dictionary of paramters.
+      Defaults to {'sigma': 1.0, 'epsilon': 1.0}
+
+    positions : array of floats. Shape = (natoms, 3)
+
+    cell: array of unit cell vectors. Shape = (3, 3)
+
+    Returns
+    -------
+    forces : an array of forces. Shape = (natoms, 3)
+
+    """
     dEdR = elementwise_grad(energy, 1)
     return -dEdR(params, positions, cell)
 
 
 def stress(params, positions, cell, strain=np.zeros((3, 3))):
-    "Compute the stress on a Lennard-Jones system."
+    """Compute the stress on a Lennard-Jones system.
+
+    Parameters
+    ----------
+
+    params : dictionary of paramters.
+      Defaults to {'sigma': 1.0, 'epsilon': 1.0}
+
+    positions : array of floats. Shape = (natoms, 3)
+
+    cell: array of unit cell vectors. Shape = (3, 3)
+
+    Returns
+    -------
+    stress : an array of stress components. Shape = (6,)
+    [sxx, syy, szz, syz, sxz, sxy]
+
+    """
     dEdst = elementwise_grad(energy, 3)
 
     volume = np.abs(np.linalg.det(cell))
